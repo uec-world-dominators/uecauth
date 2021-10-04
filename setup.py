27c3ba6253c9b4f775c68caf069975135310d452
shosatojp <me@shosato.jp>
2021-10-05 03:58:08 +0900

    readme

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@ def _requirements():
     return [name.rstrip() for name in open(os.path.join(root_dir, 'requirements.txt')).readlines()]
 
 
+def _readme():
+    with open('README.md', 'rt', encoding='utf-8') as f:
+        return f.read()
+
+
 print(setuptools.find_packages())
 setuptools.setup(
     name='uecauth',
     packages=setuptools.find_packages(),
 
-    version='0.0.4',
+    version='0.0.5',
 
     license=license,
 
@@ -26,7 +31,8 @@ setuptools.setup(
     url='https://github.com/uec-world-dominators/uecauth',
 
     description='UEC Auth',
-    long_description='',
+    long_description=_readme(),
+    long_description_content_type='text/markdown',
     keywords='',
 
     classifiers=[
